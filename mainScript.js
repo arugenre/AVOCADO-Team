323478e4f50ca2ad5a97d63cf97c31109a44853b
amen2136 <200103131@stu.sdu.edu.kz>
2021-12-17 13:56:44 +0600

    added frameworks

--- mainScript.js
+++ mainScript.js
@@ -38,4 +38,15 @@ function animated(){
 }
 setInterval(() => animated(),2000);
 
+$(".modal-button").click(function() {
+    var target = $(this).data("target");
+    $("html").addClass("is-clipped");
+    $(target).addClass("is-active");
+ });
+ 
+ $(".modal-close").click(function() {
+    $("html").removeClass("is-clipped");
+    $(this).parent().removeClass("is-active");
+ });
+
 
